1813332e3ad1d8fea495029bf03c81353a22b96c
Ron Burk <ronburk@gmail.com>
2026-09-14 23:06:18 -0700

    update messages.js

--- message.js
+++ message.js
@@ -1,31 +1,48 @@
-window.AnimScape.message = (() => {
-    const queue = [];
-    const fsm_stack = [];
-    let signal, fsm;
+/* message.js - define message loop.
+ *
+ * message = { name, command }
+ *
+ */
 
-    function start(root_fsm) {
-        fsm_stack.push(root_fsm);
-        fsm = root_fsm;
-        run_consumer();
-    }
+window.AnimScape.message = (() => {
+    const queue         = [];
+    const fsm_stack     = [];
+    let   asleep;
+    let   fsm       = (function* () { // the top-level Finite State Machine
+        let message = yield;          // caller can't pass anything on first .next() call
+        while(message !== undefined){
+            message = yield message.command();
+        }
+    })();
+    fsm.next();  // run up to first yield
 
     function send(message) {
         queue.push(message);
-        if (signal) {
-            const wake_up = signal;
-            signal = null;
-            wake_up();
+        if(asleep){
+            asleep();             // wake up the message consumer
+            asleep    = null;
         }
     }
-
-    async function run_consumer() {
+    async function start() {
         while (true) {
             while (queue.length > 0) {
                 const message = queue.shift();
-                fsm.do(message);
+                let   result  = fsm.next(message);
+                // handle state changes
+                while(result?.value?.fsm || result.done){
+                    const next_fsm = result?.value?.fsm;
+                    if(next_fsm){
+                        fsm_stack.push(fsm);
+                        fsm    = next_fsm;
+                        result = fsm.next(message);
+                    } else {
+                        console.assert(fsm_stack.length !== 0);
+                        fsm    = fsm_stack.pop();
+                        result = fsm.next(message);
+                    }
+                }
             }
-
-            await new Promise(resolve => signal = resolve);
+            await new Promise(resolve => asleep = resolve);
         }
     }
 
